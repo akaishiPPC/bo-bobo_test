d7a8093f15d92fb6758ae322ed6c059f4d6652d9
shingoAkaishi <shingo.akaishi@pripress.co.jp>
2021-11-18 11:44:58 +0900

    ぺーじ遷移の修正

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Congrats from "./components/Congrats";
 import Again from "./components/Again";
 import Nomatch from "./components/Nomatch";
+import ScrollToTop from "./components/ScrollToTop"
 import './App.scss';
 
 function App(props) {
@@ -11,22 +12,24 @@ function App(props) {
   return (
     <>
       <Router>
-        <div>
-          <Switch>
-            <Route exact path="/congrats">
-              <Congrats titlename="正解！" />
-            </Route>
-            <Route exact path="/again" component={Again}>
-              <Again titlename="残念！" />
-            </Route>
-            <Route exact path="/">
-              <Home titlename="ホーム" />
-            </Route>
-            <Route path="*">
-              <Nomatch titlename="404" />
-            </Route>
-          </Switch>
-        </div>
+        <ScrollToTop>
+          <div>
+            <Switch>
+              <Route exact path="/congrats">
+                <Congrats titlename="正解！" />
+              </Route>
+              <Route exact path="/again" component={Again}>
+                <Again titlename="残念！" />
+              </Route>
+              <Route exact path="/">
+                <Home titlename="ホーム" />
+              </Route>
+              <Route path="*">
+                <Nomatch titlename="404" />
+              </Route>
+            </Switch>
+          </div>
+        </ScrollToTop>
       </Router>
     </>
   );
